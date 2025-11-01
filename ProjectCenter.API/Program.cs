222c2e68e4799d10d036a0b300bca5d606ec5315
Кирилл Марков <kirill-markov-2020@bk.ru>
2025-11-01 19:51:36 +0300

    Регистрация контекста в API

--- ProjectCenter.API/Program.cs
+++ ProjectCenter.API/Program.cs
@@ -1,6 +1,11 @@
+
+using Microsoft.EntityFrameworkCore;
+using ProjectCenter.Infrastructure.Persistance.Contexts;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DemKursDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
