69b7591e5fbd01ee79d214981d0d98a78dc027de
twopact <artemov9321@mail.ru>
2026-06-16 22:11:13 +0300

    Добавлен Docker

--- ProjectCenter.Infrastructure/Migrations/20260615193555_Update.cs
+++ ProjectCenter.Infrastructure/Migrations/20260615193555_Update.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace ProjectCenter.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class adsdasd : Migration
+    public partial class Update : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
