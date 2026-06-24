069ceee97d348ab3e4a2818cb315d2b79d2b0b55
twopact <artemov9321@mail.ru>
2026-06-24 14:31:14 +0300

    Максимальная длина фото с 150 на 1500

--- ProjectCenter.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
+++ ProjectCenter.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
@@ -454,8 +454,8 @@ namespace ProjectCenter.Infrastructure.Migrations
                         .HasColumnType("nvarchar(20)");
 
                     b.Property<string>("Photo")
-                        .HasMaxLength(150)
-                        .HasColumnType("nvarchar(150)");
+                        .HasMaxLength(1500)
+                        .HasColumnType("nvarchar(1500)");
 
                     b.Property<string>("Surname")
                         .IsRequired()
